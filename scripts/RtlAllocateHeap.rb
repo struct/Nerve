a06a473ad3188d259ef5b80e04cd95c8704135e2
Chris <chris.rohlf@gmail.com>
2010-07-07 10:16:41 -0400

    change output_str to log_str

--- scripts/RtlAllocateHeap.rb
+++ scripts/RtlAllocateHeap.rb
@@ -1,8 +1,8 @@
 ## This script is for Win32 RtlAllocateHeap
 
 if dir.to_s =~ /enter/
-    output_str "Size requested #{@rw.process.read32(ctx.esp+12)}"
-    output_str "Heap handle is @ #{@rw.process.read32(ctx.esp+4).to_s(16)}"
+    log_str "Size requested #{@rw.process.read32(ctx.esp+12)}"
+    log_str "Heap handle is @ #{@rw.process.read32(ctx.esp+4).to_s(16)}"
 else
-    output_str "Heap chunk returned @ #{ctx.eax.to_s(16)}"
+    log_str "Heap chunk returned @ #{ctx.eax.to_s(16)}"
 end

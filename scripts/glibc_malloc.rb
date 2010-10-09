86697ae141ad998ae9d2255dd6784da13db3e1ea
Chris <chris.rohlf@gmail.com>
2010-10-09 15:47:26 -0400

    updates to example scripts & configurations

--- scripts/glibc_malloc.rb
+++ scripts/glibc_malloc.rb
@@ -5,12 +5,14 @@
 r = @ragweed.get_registers
 
 puts "-----------------"
-puts sprintf "eax = %08x\nebx = %08x\necx = %08x\nedx = %08x\nesp = %08x\nebp = %08x\neip = %08x\n",
-    r[:eax], r[:ebx], r[:ecx], r[:edx], r[:esp], r[:ebp], r[:eip]
+#puts sprintf "eax = %08x\nebx = %08x\necx = %08x\nedx = %08x\nesp = %08x\nebp = %08x\neip = %08x\n",
+#    r[:eax], r[:ebx], r[:ecx], r[:edx], r[:esp], r[:ebp], r[:eip]
+@ragweed.print_registers
 
 esp = Ragweed::Wraptux::ptrace(Ragweed::Wraptux::Ptrace::PEEK_TEXT, @pid, r[:esp]+4, 0)
 @log.str "malloc(#{esp})"
 
+#locs = @ragweed.search_process(0x41414141)
 locs = @ragweed.search_heap(0x41414141)
 
 if !locs.empty?
